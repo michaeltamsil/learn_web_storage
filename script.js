72510f4db89a61e6e3eb9aa59cf3d0ec3961d7e6
michael tamsil <michaeltamsil@gmail.com>
2020-04-03 12:57:29 +0700

    add hapus

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const addItemInHTML = value => {
 
+const hapus = () => {
+    $(event.currentTarget).parent().remove();
+    //debugger;
+    // alert('mau hapus');
+}
+
+const addItemInHTML = function(value) {
     if (value == null) {
         return ;
     }
@@ -7,7 +13,7 @@ const addItemInHTML = value => {
     const daftarList = document.getElementsByClassName("daftar-list")
 
     const li = document.createElement("li")
-    const itemHTML =`${value} <button>hapus</button>`;
+    const itemHTML =`${value} <button onclick="hapus()">hapus</button>`;
 
     daftarList[0].appendChild(li)
     li.innerHTML = itemHTML
@@ -16,11 +22,8 @@ const addItemInHTML = value => {
 const addItem = () => {
     const itemInput = document.getElementsByName("itemInput")
     let itemInputValue = itemInput[0].value
-    if (!itemInputValue){
-        return ;    // gunanya utk exit
-    }
-
     addItemInHTML(itemInputValue)
+
     let itemArray = localStorage.getItem("item");
 
     if ( itemArray == null ){
@@ -46,7 +49,6 @@ const runNow = () => {
     const itemFromLS = localStorage.getItem("item")
     
     if (itemFromLS != null){
-        debugger;
         const items = JSON.parse(itemFromLS);
         
         items.forEach( item => {
@@ -56,4 +58,99 @@ const runNow = () => {
     
 }
 
+
 runNow();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const logMessage = function(pesan, param2) {
+    console.log(pesan);
+}
+// bonus internet 44.31GB, midnight quota 44.98 GB
+const logMessage2 = (pesan) => {
+    console.log(pesan)
+}
+
+const logMessage3 = (pesan, jumlah) => {
+    console.log(`${pesan} dgn jumlah ${jumlah}`)
+    window.alert(`${pesan} dgn jumlah ${jumlah}`)
+}
+
+const logMessage4 = pesan => console.log(pesan)
+
+
+// contoh penggunaan return di function
+const multiple = function(number){
+    return number * number;
+}
+//debugger;
+const nilai = multiple(5);
+console.log(`nilai multiple dari 5 adalah ${nilai}`);
+
+
+const multiple2 = number => number * number ;
+
+const nilai2 = multiple2(6);
+console.log(`nilai multiple2 dari 6 adalah ${nilai2}`);
+
+
+const inputProduct = (productName = "", number = 1) => {
+    console.log(`product name ${productName}, with ${number} pieces`)
+}
+
+const isLulus = (nilai = 0) => {
+    console.log(nilai)
+    if (nilai >= 80)
+        return true;
+    else {
+        return false;
+    }
+}
+
+const nilaiSaya = 100;
+let lulus = isLulus(nilaiSaya);
+console.log(`dengan nilai saya ${nilaiSaya}, apakah saya lulus?`);
+let kelulusan = ( lulus ? 'lulus': 'ngulang' );
+console.log(kelulusan);
+
+
+
+// contoh callback function
+const doWork = () => {
+    console.log('bathing');
+    console.log('clothing');
+    console.log('to office');
+}
+
+const doSleep = () => {
+    console.log('bathing');
+    console.log('eating');
+    console.log('go to kasur');
+}
+
+const ask = (message, doIt, somethingElse) => {
+    const isSetuju = window.confirm(message);
+    if (isSetuju){
+        doIt();
+    }else {
+        somethingElse();
+    }
+}
+
+// ask("do you work?", doWork, doSleep);
